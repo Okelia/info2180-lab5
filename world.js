17c57b95e2d9d22b2a5274838cfda6d5779c0af8
Okelia <greenokelia@gmail.com>
2020-11-18 00:04:29 -0800

    Display Additional Countries Information in a Table

--- world.js
+++ world.js
@@ -13,6 +13,20 @@ document.addEventListener("DOMContentLoaded", ()=>{
         httprequest.open("GET", url, true);
         httprequest.send("");
     }
+    /*var btn_cities = document.getElementById("lookup_cities").onclick= function(e){
+        var coun = document.getElementById("country").value;
+        var printResult = document.getElementById("result");
+        var httprequest = new XMLHttpRequest();
+        var url = "world.php?country=" + coun ;
+        httprequest.onreadystatechange = function(){
+            if (this.status ==200){
+                document.getElementById("result").innerHTML =  this.response;
+                //alert(this.response);
+            }
+        };
+        httprequest.open("GET", url, true);
+        httprequest.send("");
+    }*/
     
     /*function searchCountry(){ 
         
--- world.php
+++ world.php
@@ -4,22 +4,13 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 $country =  $_GET['country'];
-
-/*if (!empty($_GET['all'])){
-  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-  $stmt = $conn->query("SELECT * FROM countries");
-  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  echo "</ul";
-  foreach ($results as $row):
-    echo "<li>" . $row['name'] . ' is ruled by ' . $row['head_of_state'] . "</li>";
-  endforeach;
-  echo "</ul";
-}*/
+//$cities =  $_GET['cities'];
 
 if (isset($_GET['country']) || !empty($_GET['country'])){
   $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
   $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';"); 
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
   echo "<table border ='1'>
   <tr>
   <th> Name </th>
@@ -37,6 +28,25 @@ if (isset($_GET['country']) || !empty($_GET['country'])){
   endforeach;
   echo "</table>";
 }
+/*if (isset($_GET['cities']) || !empty($_GET['cities'])){
+  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+  $stmt = $conn->query("SELECT * FROM cities");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  echo "<table border ='1'>
+  <tr>
+  <th> Name </th>
+  <th> District </th>
+  <th> Population </th>
+  </tr>";
+  foreach ($results as $row):
+    echo "<tr>";
+    echo "<td>".$row['name'] ."</td>";
+    echo "<td>".$row['district'] ."</td>";
+    echo "<td>".$row['Population'] ."</td>";
+    echo "<tr>";
+  endforeach;
+  echo "</table>";
+}*/
 
 /*?>
 <ul>
