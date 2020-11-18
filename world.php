862d558fec2970b5a12f10b5319c29e1e91af29c
Okelia <greenokelia@gmail.com>
2020-11-17 21:07:06 -0800

    Display Additional Countries Information in a Table

--- world.php
+++ world.php
@@ -5,7 +5,7 @@ $password = 'password123';
 $dbname = 'world';
 $country =  $_GET['country'];
 
-if (!empty($_GET['all'])){
+/*if (!empty($_GET['all'])){
   $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
   $stmt = $conn->query("SELECT * FROM countries");
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -14,19 +14,29 @@ if (!empty($_GET['all'])){
     echo "<li>" . $row['name'] . ' is ruled by ' . $row['head_of_state'] . "</li>";
   endforeach;
   echo "</ul";
-}
+}*/
 
 if (isset($_GET['country']) || !empty($_GET['country'])){
   $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
   $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';"); 
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  echo "</ul";
+  echo "<table border ='1'>
+  <tr>
+  <th> Name </th>
+  <th> Continent </th>
+  <th> Independence </th>
+  <th> Head of State</th>
+  </tr>";
   foreach ($results as $row):
-    echo "<li>" . $row['name'] . ' is ruled by ' . $row['head_of_state'] . "</li>";
+    echo "<tr>";
+    echo "<td>".$row['name'] ."</td>";
+    echo "<td>".$row['continent'] ."</td>";
+    echo "<td>".$row['Independence Year'] ."</td>";
+    echo "<td>".$row['head_of_state'] . "</td>";
+    echo "<tr>";
   endforeach;
-  echo "</ul";
+  echo "</table>";
 }
-
 
 /*?>
 <ul>
