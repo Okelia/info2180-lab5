6be6a84174a873426db5737d94c10010d84fd52d
Okelia <greenokelia@gmail.com>
2020-11-17 20:00:09 -0800

    Added Look up any Country

---
+++ world.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    var btn = document.getElementById("lookup").onclick= function(e){
+        var coun = document.getElementById("country").value;
+        var printResult = document.getElementById("result");
+        var httprequest = new XMLHttpRequest();
+        var url = "world.php?country=" + coun;
+        httprequest.onreadystatechange = function(){
+            if (this.status ==200){
+                document.getElementById("result").innerHTML =  this.response;
+                //alert(this.response);
+            }
+        };
+        httprequest.open("GET", url, true);
+        httprequest.send("");
+    }
+    
+    /*function searchCountry(){ 
+        
+        httprequest.onreadystatechange = function(){
+            if (this.status ==200){
+                this.response = document.getElementById("result").innerHTML;
+                alert(this.response);
+            }
+        };
+        httprequest.open("GET", url, true);
+        httprequest.send("");
+    }*/
+})
--- world.php
+++ world.php
@@ -1,17 +1,36 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
+$country =  $_GET['country'];
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+if (!empty($_GET['all'])){
+  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+  $stmt = $conn->query("SELECT * FROM countries");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  echo "</ul";
+  foreach ($results as $row):
+    echo "<li>" . $row['name'] . ' is ruled by ' . $row['head_of_state'] . "</li>";
+  endforeach;
+  echo "</ul";
+}
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if (isset($_GET['country']) || !empty($_GET['country'])){
+  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';"); 
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  echo "</ul";
+  foreach ($results as $row):
+    echo "<li>" . $row['name'] . ' is ruled by ' . $row['head_of_state'] . "</li>";
+  endforeach;
+  echo "</ul";
+}
 
-?>
+
+/*?>
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
-</ul>
+</ul>*/
